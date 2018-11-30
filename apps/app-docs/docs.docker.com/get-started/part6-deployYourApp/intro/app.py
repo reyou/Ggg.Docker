8e8c261429346298941da89e5f482b134a21a1b2
Teoman Shipahi <aytekoz@yahoo.com>
2018-11-29 23:58:41 -0500

    share your image

--- apps/app-docs/docs.docker.com/get-started/part6-deployYourApp/intro/app.py
+++ apps/app-docs/docs.docker.com/get-started/part6-deployYourApp/intro/app.py
@@ -16,10 +16,10 @@ def hello():
         # https://www.programcreek.com/python/example/65603/redis.RedisError
         visits = "<i>cannot connect to Redis, counter disabled</i>" + " " + "Err: {}".format(ex)
 
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Hello there! Welcome {name} - {version}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "world"), version=os.getenv("VERSION","VersionNotSet"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
